7d6516760fcdcbaf3a5659aabbab7d61b3ec64f6
Michael <shibut.m.v@edu.mirea.ru>
2025-02-25 00:48:27 +0300

    Damn, I finished that

---
+++ blogicum/core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def page_not_found(request, exception):
+    # Переменная exception содержит отладочную информацию;
+    # выводить её в шаблон пользовательской страницы 404 мы не станем.
+    return render(request, 'core/404.html', status=404)
+
+
+def csrf_failure(request, reason=''):
+    return render(request, 'core/403csrf.html', status=403)
+
+
+# def internal_server_error(request, exception):
+#     return render(request, 'core/500.html', status=500)
